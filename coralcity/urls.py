30648157665aef64c3471b7f87dc4eb7c8710480
LeTHaLOp17 <aksingh6224@gmail.com>
2025-04-25 10:18:56 +0530

    readme

--- coralcity/urls.py
+++ coralcity/urls.py
@@ -1,18 +1,4 @@
-"""btre URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
 from graphene_django.views import GraphQLView
@@ -40,7 +26,7 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
-admin.site.site_header ="Koralcity control panel"
+admin.site.site_header ="Sky Livings panel"
 admin.site.index_title ="Administrators dashboard"
 admin.site.site_title ="Control panel"
 
